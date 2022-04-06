6c3ce32ea3f190e09637cd26646a0d34ff510f93
Danil Sitdikov <quadrad4@gmail.com>
2022-04-06 18:17:45 +0600

    feat KLD-44 - kaikas integration

---
+++ components/button/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click="$emit('click')" class="btn">
+    <slot></slot>
+  </button>
+</template>
+
+<style>
+.btn {
+  background: #D0021B;
+  border: 0;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 12px;
+  color: #fff;
+  border-radius: 4px;
+  padding: 9px 13px;
+  cursor: pointer;
+}
+</style>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: {
+      type: String,
+      isRequire: true,
+    }
+  }
+}
+</script>
