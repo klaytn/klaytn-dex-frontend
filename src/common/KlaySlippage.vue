0308cda17072dd1e2e474eda457754fddb9e70a2
RDMStreet <RDMStreetOne@gmail.com>
2022-06-23 21:56:55 +0300

    [Refactor] Enable ts for all components

--- src/common/KlaySlippage.vue
+++ src/common/KlaySlippage.vue
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 import { mapActions, mapState } from 'pinia'
 
 export default {
--- src/modules/SwapModule/ExchangeRate.vue
+++ src/modules/SwapModule/ExchangeRate.vue
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 import debounce from 'debounce'
 import { mapActions, mapState } from 'pinia'
 
