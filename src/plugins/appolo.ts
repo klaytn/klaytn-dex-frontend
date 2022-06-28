758a1f6ec192f13c41c3933777eae1f9dbe42227
Danil Sitdikov <quadrad4@gmail.com>
2022-06-28 14:29:30 +0600

    feat[KLD-74]: LP. Subgraph integration

---
+++ src/plugins/appolo.ts
@@ -0,0 +1,18 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+import type { Plugin } from '@/types'
+
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+  cache,
+  uri: 'https://graph.ipfs1.dev.infra.soramitsu.co.jp/subgraphs/name/klaytn-subgraph/exchange',
+})
+const apolloProvider = createApolloProvider({
+  defaultClient: apolloClient,
+})
+
+export const install: Plugin = ({ app }) => {
+  app.use(apolloProvider)
+  app.use(apolloClient)
+}
