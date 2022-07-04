b311b726e3f7f8d18581b6e412528ce8a0008ad9
0x009922 <a.marcius26@gmail.com>
2022-07-04 11:42:47 +0600

    [fix]: commit utils too

---
+++ src/utils/common.ts
@@ -0,0 +1,42 @@
+import BigNumber from 'bignumber.js'
+
+// FIXME v1 & v2 values comes from `Token.value`. What type is it?
+
+export function formatRate(v1: string, v2: string) {
+  const bigNA = new BigNumber(v1)
+  const bigNB = new BigNumber(v2)
+
+  return bigNA.dividedBy(bigNB).toFixed(5)
+}
+
+export function formatPercent(v1: string, v2: string) {
+  const bigNA = new BigNumber(v1)
+  const bigNB = new BigNumber(v2)
+  const percent = bigNA.dividedToIntegerBy(100)
+
+  return `${bigNB.dividedBy(percent).toFixed(2)}%`
+}
+
+if (import.meta.vitest) {
+  const { test, expect, describe } = import.meta.vitest
+
+  describe('format rate', () => {
+    test('case 1', () => {
+      expect(formatRate('423', '20')).toMatchInlineSnapshot('"21.15000"')
+    })
+
+    test('case 2', () => {
+      expect(formatRate('1000', '3.5')).toMatchInlineSnapshot('"285.71429"')
+    })
+  })
+
+  describe('format percent', () => {
+    test('case 1', () => {
+      expect(formatPercent('423', '20')).toMatchInlineSnapshot('"5.00%"')
+    })
+
+    test('case 2', () => {
+      expect(formatPercent('1000', '3.5')).toMatchInlineSnapshot('"0.35%"')
+    })
+  })
+}
