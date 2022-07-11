1b1293ee6389d12f3bb95713bad16466ed628481
RDMStreet <RDMStreetOne@gmail.com>
2022-07-11 18:31:05 +0300

    [Fix] Changed params of hslGenerator

--- src/common/KlayIcon.vue
+++ src/common/KlayIcon.vue
@@ -9,14 +9,13 @@ const char = computed(() => {
   return symbol?.value?.[0] ?? ''
 })
 
-function stringToHslColor(str: string, s: number, l: number) {
-  let hash = 0
-  for (let i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash)
-  }
-  
-  let h = hash % 400 // 460
-  return 'hsl('+h+', '+s+'%, '+l+'%)'
+const hslGenerator = (str: string, s: number, l: number) => {
+  const [...strArray] = str
+  const hash = strArray.reduce((a, c) => {
+    const h = c.charCodeAt(0) + ((a << 4) - a)
+    return h % 360
+  }, 0)
+  return `hsl(${hash}, ${s}%, ${l}%)`
 }
 
 const CurrentIcon = defineAsyncComponent(() => import(`../assets/icons/${name.value}.svg`))
@@ -26,7 +25,7 @@ const CurrentIcon = defineAsyncComponent(() => import(`../assets/icons/${name.va
   <div
     v-if="symbol"
     class="char"
-    :style="{ background: stringToHslColor(symbol, 80, 70) }"
+    :style="{ background: hslGenerator(symbol, 80, 70) }"
   >
     {{ char }}
   </div>
