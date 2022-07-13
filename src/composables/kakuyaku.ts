93476522060e1433047d4eb85c64f3a41ad66033
0x009922 <a.marcius26@gmail.com>
2022-07-13 16:38:11 +0600

    [refactor]: try new design

--- src/composables/kakuyaku.ts
+++ src/composables/kakuyaku.ts
@@ -1,33 +1,152 @@
-import { Task, wheneverTaskErrors } from '@vue-kakuyaku/core'
 import { Status } from '@soramitsu-ui/ui'
 import Debug from 'debug'
+import { Except } from 'type-fest'
+import { Ref, WatchOptions, WatchStopHandle } from 'vue'
 
-export function useTaskLog(task: Task<unknown>, name: string) {
-  const debug = Debug('kakuyaku').extend(name)
+type Nullable<T> = null | undefined | T
 
-  watchEffect(() => {
-    const state = task.state
-    if (state.kind !== 'uninit') {
-      if (state.kind === 'ok') {
-        debug('ok: %o', state.data)
-      } else if (state.kind === 'err') {
-        debug('err: %o', state.error)
-        console.error(`Task "${name}" errored:`, state.error)
-      } else if (state.kind === 'pending') {
-        debug('pending...')
-      } else {
-        debug('aborted')
+type PromiseResult<T> = { kind: 'fulfilled'; value: T } | { kind: 'rejected'; reason: unknown }
+
+type PromiseState<T> = { kind: 'pending' } | PromiseResult<T>
+
+interface UsePromiseReturn<T> {
+  state: Ref<null | PromiseState<T>>
+  set: (promise: Promise<T>) => void
+  clear: () => void
+}
+
+function resultRaw<T>(result: PromiseResult<T>): PromiseResult<T> {
+  return markRaw(result)
+}
+
+type WheneverPromiseOptions = Except<WatchOptions, 'deep'>
+
+export function wheneverFulfilled<T>(
+  state: Ref<Nullable<PromiseState<T>>>,
+  fn: (value: T) => void,
+  options?: WheneverPromiseOptions,
+): WatchStopHandle {
+  return watch(
+    state,
+    (state) => {
+      if (state?.kind === 'fulfilled') {
+        fn(state.value)
       }
-    }
-  })
+    },
+    options,
+  )
+}
 
-  onScopeDispose(() => {
-    debug('disposed')
+export function wheneverRejected(
+  state: Ref<Nullable<PromiseState<unknown>>>,
+  fn: (reason: unknown) => void,
+  options?: WheneverPromiseOptions,
+): WatchStopHandle {
+  return watch(
+    state,
+    (state) => {
+      if (state?.kind === 'rejected') {
+        fn(state.reason)
+      }
+    },
+    options,
+  )
+}
+
+export function wheneverDone<T>(
+  state: Ref<Nullable<PromiseState<T>>>,
+  fn: (result: PromiseResult<T>) => void,
+  options?: WheneverPromiseOptions,
+): WatchStopHandle {
+  return watch(
+    state,
+    (state) => {
+      if (state && state.kind !== 'pending') {
+        fn(state)
+      }
+    },
+    options,
+  )
+}
+
+export function usePromise<T>(): UsePromiseReturn<T> {
+  let active: null | Promise<T> = null
+  const state = shallowRef<null | PromiseState<T>>(null)
+
+  function set(promise: Promise<T>) {
+    active = promise
+
+    state.value = { kind: 'pending' }
+
+    promise
+      .then((value) => {
+        if (promise === active) {
+          state.value = resultRaw({ kind: 'fulfilled', value })
+        }
+      })
+      .catch((reason) => {
+        if (promise === active) {
+          state.value = resultRaw({ kind: 'rejected', reason })
+        } else {
+          // to not silent it
+          throw reason
+        }
+      })
+  }
+
+  function clear() {
+    active = null
+  }
+
+  return { state, set, clear }
+}
+
+export interface PromiseStateFlatten<T> {
+  pending: boolean
+  fulfilled: null | { value: T }
+  rejected: null | { reason: unknown }
+}
+
+export function useFlattenState<T>(state: Ref<Nullable<PromiseState<T>>>): PromiseStateFlatten<T> {
+  return reactive({
+    pending: computed(() => state.value?.kind === 'pending'),
+    fulfilled: computed(() => (state.value?.kind === 'fulfilled' ? markRaw({ value: state.value.value }) : null)),
+    rejected: computed(() => (state.value?.kind === 'rejected' ? markRaw({ reason: state.value.reason }) : null)),
   })
 }
 
-export function useNotifyOnError(task: Task<unknown>, message: string) {
-  wheneverTaskErrors(task, () => {
+export interface PromiseStaleState<T> extends PromiseStateFlatten<T> {
+  fresh: boolean
+}
+
+export function useStaleState<T>(state: Ref<Nullable<PromiseState<T>>>): PromiseStaleState<T> {
+  const stale: PromiseStaleState<T> = reactive({})
+
+  watch(state, (state) => {}, { flush: 'sync' })
+
+  return stale
+}
+
+export function usePromiseLog(promiseState: Ref<Nullable<PromiseState<unknown>>>, name: string) {
+  const debug = Debug('kakuyaku').extend(name)
+
+  watchEffect(() => {
+    const state = promiseState.value
+    if (state) {
+      if (state.kind === 'fulfilled') {
+        debug('fulfilled: %o', state.value)
+      } else if (state.kind === 'rejected') {
+        debug('rejected: %o', state.reason)
+        console.error(`Promise "${name}" errored:`, state.reason)
+      } else if (state.kind === 'pending') {
+        debug('pending...')
+      }
+    }
+  })
+}
+
+export function useNotifyOnError(state: Ref<Nullable<PromiseState<unknown>>>, message: string) {
+  wheneverRejected(state, () => {
     $notify({ status: Status.Error, description: message })
   })
 }
--- src/modules/ModuleEarnShared/composable.fetch-rewards.ts
+++ src/modules/ModuleEarnShared/composable.fetch-rewards.ts
@@ -1,7 +1,7 @@
 import { Kaikas, Address, asWei } from '@/core/kaikas'
-import { useScope, useStaleIfErrorState, useTask, wheneverTaskSucceeds } from '@vue-kakuyaku/core'
 import { MULTICALL } from '@/core/kaikas/smartcontracts/abi'
 import { Multicall } from '@/types/typechain/farming/MultiCall.sol'
+import invariant from 'tiny-invariant'
 import { Ref } from 'vue'
 import { MULTICALL_CONTRACT_ADDRESS, REFETCH_REWARDS_INTERVAL } from './const'
 import { PoolId, Rewards } from './types'
@@ -24,48 +24,46 @@ export function useFetchRewards<T extends PoolId | Address>({
 } {
   const MulticallContract = kaikas.cfg.createContract<Multicall>(MULTICALL_CONTRACT_ADDRESS, MULTICALL)
 
-  const maybeStaleState = useScope(
-    computed(() => !!poolIds.value),
-    () => {
-      const task = useTask(async () => {
-        const ids = poolIds.value!
-        const calls = prepareCalls(ids)
-        const aggrResult = await MulticallContract.methods.aggregate(calls).call()
+  async function fetchRewards(ids: T[]): Promise<FetchRewardsResult> {
+    const calls = prepareCalls(ids)
+    const aggrResult = await MulticallContract.methods.aggregate(calls).call()
 
-        // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-        const rewards = {} as Rewards<T>
-        aggrResult.returnData.forEach((hex, idx) => {
-          rewards[ids[idx]] = asWei(kaikas.cfg.caver.klay.abi.decodeParameter('uint256', hex))
-        })
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    const rewards = {} as Rewards<T>
+    aggrResult.returnData.forEach((hex, idx) => {
+      rewards[ids[idx]] = asWei(kaikas.cfg.caver.klay.abi.decodeParameter('uint256', hex))
+    })
 
-        return {
-          rewards,
-          blockNumber: Number(aggrResult.blockNumber),
-        }
-      })
+    return {
+      rewards,
+      blockNumber: Number(aggrResult.blockNumber),
+    }
+  }
 
-      function run() {
-        if (task.state.kind !== 'pending') task.run()
-      }
-      const runDebounced = useDebounceFn(run, REFETCH_REWARDS_INTERVAL)
+  interface FetchRewardsResult {
+    rewards: Rewards<T>
+    blockNumber: number
+  }
 
-      useTaskLog(task, 'fetch-rewards-generic')
-      watch(poolIds, run)
-      whenever(
-        () => task.state.kind === 'ok' || task.state.kind === 'err',
-        () => runDebounced(),
-      )
-      wheneverTaskSucceeds(task, (result) => updateBlockNumber(result.blockNumber))
+  const promise = usePromise<FetchRewardsResult>() // use promise state
 
-      run()
+  function run() {
+    const ids = poolIds.value
+    invariant(ids)
+    if (promise.state.value?.kind !== 'pending') {
+      promise.set(fetchRewards(ids)) // set promise
+    }
+  }
+  const runDebounced = useDebounceFn(run, REFETCH_REWARDS_INTERVAL)
 
-      return useStaleIfErrorState(task)
-    },
-  )
+  watch(poolIds, run)
+  wheneverDone(promise.state, runDebounced)
+  wheneverFulfilled(promise.state, ({ blockNumber }) => updateBlockNumber(blockNumber))
+  usePromiseLog(promise.state, 'fetch-rewards-generic')
 
-  const rewards = computed(() => {
-    return maybeStaleState.value?.setup.result?.some?.rewards ?? null
-  })
+  const fulfilled = toRef(useStaleState(promise.state), 'fulfilled') // getting stale rewards data
+
+  const rewards = computed(() => fulfilled.value?.value?.rewards ?? null)
   const areRewardsFetched = computed(() => !!rewards.value)
 
   return { rewards, areRewardsFetched }
