a2729021cfa409c7ae932e91f1f90dd8bb6e762e
0x009922 <a.marcius26@gmail.com>
2022-07-14 16:26:35 +0600

    [refactor]: refactor composables

--- src/modules/ModuleEarnShared/composable.check-enabled.ts
+++ src/modules/ModuleEarnShared/composable.check-enabled.ts
@@ -1,41 +1,42 @@
 import { Address, asWei } from '@/core/kaikas'
-import { useTask, wheneverTaskErrors } from '@vue-kakuyaku/core'
-import { Status } from '@soramitsu-ui/ui'
 import BigNumber from 'bignumber.js'
 import { MAX_UINT256 } from './const'
-import { MaybeRef } from '@vueuse/core'
+import { MaybeRef, or } from '@vueuse/core'
 
 export function useEnableState(addr: MaybeRef<Address>, contractAddr: MaybeRef<Address>) {
   const kaikasStore = useKaikasStore()
 
-  const checkEnabledTask = useTask(async () => {
+  async function checkFn(): Promise<boolean> {
     const allowance = await kaikasStore.getKaikasAnyway().cfg.getAllowance(unref(addr), unref(contractAddr))
     const isEnabled = new BigNumber(allowance).isEqualTo(MAX_UINT256)
     return isEnabled
-  })
-  useTaskLog(checkEnabledTask, 'use-enable-state-check')
-  wheneverTaskErrors(checkEnabledTask, () => {
-    $notify({ status: Status.Error, description: 'Fetch enabled pools error' })
-  })
-  const checkEnabledInProgress = computed(() => checkEnabledTask.state.kind === 'pending')
+  }
 
-  const enableTask = useTask(async () => {
+  async function enableFn() {
     const kaikas = kaikasStore.getKaikasAnyway()
     await kaikas.cfg.approveAmount(unref(addr), asWei(MAX_UINT256.toFixed()), unref(contractAddr))
-  })
-  useTaskLog(enableTask, 'use-enable-state-enable')
-  wheneverTaskErrors(enableTask, () => {
-    $notify({ status: Status.Error, description: 'Approve amount error' })
-  })
+  }
 
-  const enabled = computed(() => {
-    return enableTask.state.kind === 'ok' || (checkEnabledTask.state.kind === 'ok' && checkEnabledTask.state.data)
-  })
+  const checkPromise = usePromise<boolean>()
+  const isCheckPending = toRef(checkPromise.state, 'pending')
+  useNotifyOnError(checkPromise.state, 'Fetch enabled pools error')
+  function check() {
+    checkPromise.set(checkFn())
+  }
+
+  const enablePromise = usePromise()
+  const isEnablePending = toRef(enablePromise.state, 'pending')
+  useNotifyOnError(enablePromise.state, 'Fetch enabled pools error')
+  function enable() {
+    enablePromise.set(enableFn())
+  }
+
+  const isEnabled = computed(() => !!enablePromise.state.fulfilled && !!checkPromise.state.fulfilled?.value)
 
   return {
-    pending: checkEnabledInProgress,
-    check: () => checkEnabledTask.run(),
-    enable: () => enableTask.run(),
-    enabled,
+    pending: or(isCheckPending, isEnablePending),
+    check,
+    enable,
+    enabled: isEnabled,
   }
 }
