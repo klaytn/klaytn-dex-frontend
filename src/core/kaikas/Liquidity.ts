cb0bb2f4c1780128316bbc53a5ace05bd3371733
0x009922 <a.marcius26@gmail.com>
2022-07-14 12:24:33 +0600

    [refactor]: refactor liquidity core

--- src/core/kaikas/Liquidity.ts
+++ src/core/kaikas/Liquidity.ts
@@ -1,18 +1,14 @@
 import Config from './Config'
 import { type ValueWei, type Address, type Deadline, WeiNumStrBn } from './types'
 import BN from 'bn.js'
-import { MAGIC_GAS_PRICE } from './const'
-import { buildPairAsync, TokensPair } from '@/utils/pair'
-import { asWei, deadlineFiveMinutesFromNow, isNativeToken, tokenRawToWei } from './utils'
+import { MAGIC_GAS_PRICE, NATIVE_TOKEN } from './const'
+import { buildPair, buildPairAsync, TokensPair } from '@/utils/pair'
+import { asWei, deadlineFiveMinutesFromNow, isNativeToken } from './utils'
 import { DexPair } from '@/types/typechain/swap'
 import { PAIR } from './smartcontracts/abi'
 import Tokens from './Tokens'
 
-export interface AddLiquidityResult {
-  lpTokenGas: ValueWei<number>
-}
-
-export interface AddLiquidityProps {
+export interface PrepareAddLiquidityProps {
   tokens: TokensPair<TokenAddressAndDesiredValue>
   deadline: Deadline
 }
@@ -36,10 +32,18 @@ export interface ComputeRemoveLiquidityAmountsResult {
   totalSupply: ValueWei<BN>
 }
 
-export interface RemoveLiquidityProps {
+export interface PrepareRemoveLiquidityProps {
   tokens: TokensPair<Address>
   pair: Address
   lpTokenValue: WeiNumStrBn
+}
+
+export interface PrepareTransactionResult {
+  /**
+   * i.e. transaction fee
+   */
+  gas: ValueWei<number>
+  send: () => Promise<void>
 }
 
 function minByDesired(desired: WeiNumStrBn): ValueWei<string> {
@@ -48,22 +52,15 @@ function minByDesired(desired: WeiNumStrBn): ValueWei<string> {
   return asWei(nDesired.sub(nDesired.divn(100)).toString())
 }
 
-function detectEth(
-  tokens: TokensPair<TokenAddressAndDesiredValue>,
-): null | { token: TokenAddressAndDesiredValue; desiredEth: WeiNumStrBn } {
-  if (isNativeToken(tokens.tokenA.addr)) return { token: tokens.tokenB, desiredEth: tokens.tokenA.desired }
-  if (isNativeToken(tokens.tokenB.addr)) return { token: tokens.tokenA, desiredEth: tokens.tokenB.desired }
+function detectEth<T extends { addr: Address }>(
+  pair: TokensPair<T>,
+): null | {
+  token: T
+  eth: T
+} {
+  if (isNativeToken(pair.tokenA.addr)) return { token: pair.tokenB, eth: pair.tokenA }
+  if (isNativeToken(pair.tokenB.addr)) return { token: pair.tokenA, eth: pair.tokenB }
   return null
-}
-
-/**
- * FIXME
- */
-function rmLiquidityLpToken(props: RemoveLiquidityProps): ValueWei<BN> {
-  if (isNativeToken(props.tokens.tokenA) || isNativeToken(props.tokens.tokenB)) {
-    return asWei(new BN(tokenRawToWei({ decimals: 18 }, '10')))
-  }
-  return asWei(new BN(props.lpTokenValue))
 }
 
 export default class Liquidity {
@@ -88,7 +85,7 @@ export default class Liquidity {
    * - Detects whether to add liquidity for ETH or not
    * - Computes minimal amount as 99% of desired value
    */
-  public async addLiquidity(props: AddLiquidityProps): Promise<AddLiquidityResult> {
+  public async prepareAddLiquidity(props: PrepareAddLiquidityProps): Promise<PrepareTransactionResult> {
     for (const type of ['tokenA', 'tokenB'] as const) {
       const { addr, desired } = props.tokens[type]
       await this.cfg.approveAmount(addr, desired)
@@ -96,7 +93,10 @@ export default class Liquidity {
 
     const detectedEth = detectEth(props.tokens)
     if (detectedEth) {
-      const { token, desiredEth } = detectedEth
+      const {
+        token,
+        eth: { desired: desiredEth },
+      } = detectedEth
 
       const method = this.router.methods.addLiquidityETH(
         token.addr,
@@ -114,14 +114,16 @@ export default class Liquidity {
           value: desiredEth,
         }),
       )
-      await method.send({
-        from: this.addr,
-        gas: lpTokenGas,
-        gasPrice: MAGIC_GAS_PRICE,
-        value: desiredEth,
-      })
+      const send = async () => {
+        await method.send({
+          from: this.addr,
+          gas: lpTokenGas,
+          gasPrice: MAGIC_GAS_PRICE,
+          value: desiredEth,
+        })
+      }
 
-      return { lpTokenGas }
+      return { gas: lpTokenGas, send }
     } else {
       const { tokenA, tokenB } = props.tokens
 
@@ -137,37 +139,58 @@ export default class Liquidity {
       )
 
       const lpTokenGas = asWei(await method.estimateGas())
-      await method.send({
-        from: this.addr,
-        gas: lpTokenGas,
-        gasPrice: MAGIC_GAS_PRICE,
-      })
+      const send = async () => {
+        await method.send({
+          from: this.addr,
+          gas: lpTokenGas,
+          gasPrice: MAGIC_GAS_PRICE,
+        })
+      }
 
-      return { lpTokenGas }
+      return { gas: lpTokenGas, send }
     }
   }
 
   /**
-   * FIXME there should be a different between ETH/nonETH, right?
+   * - Approves that pair has enought amount for `lpTokenValue`
    */
-  public async rmLiquidity(props: RemoveLiquidityProps): Promise<void> {
-    const nLpToken = rmLiquidityLpToken(props)
-    const { amounts } = await this.computeRmLiquidityAmounts(props)
-
+  public async prepareRmLiquidity(props: PrepareRemoveLiquidityProps): Promise<PrepareTransactionResult> {
+    const nLpToken = asWei(new BN(props.lpTokenValue))
     await this.cfg.approveAmount(props.pair, nLpToken)
 
-    const method = this.router.methods.removeLiquidity(
-      props.tokens.tokenA,
-      props.tokens.tokenB,
-      nLpToken,
-      amounts.tokenA,
-      amounts.tokenB,
-      this.addr,
-      deadlineFiveMinutesFromNow(),
-    )
+    const { amounts } = await this.computeRmLiquidityAmounts({ ...props, lpTokenValue: nLpToken })
 
-    const gas = await method.estimateGas({ from: this.addr, gasPrice: MAGIC_GAS_PRICE })
-    await method.send({ from: this.addr, gas, gasPrice: MAGIC_GAS_PRICE })
+    const detectedEth = detectEth(
+      buildPair((type) => ({
+        addr: props.tokens[type],
+        computedAmount: amounts[type],
+      })),
+    )
+    const method = detectedEth
+      ? this.router.methods.removeLiquidityETH(
+          detectedEth.token.addr,
+          nLpToken,
+          detectedEth.token.computedAmount,
+          detectedEth.eth.computedAmount,
+          this.addr,
+          deadlineFiveMinutesFromNow(),
+        )
+      : this.router.methods.removeLiquidity(
+          props.tokens.tokenA,
+          props.tokens.tokenB,
+          nLpToken,
+          amounts.tokenA,
+          amounts.tokenB,
+          this.addr,
+          deadlineFiveMinutesFromNow(),
+        )
+
+    const gas = asWei(await method.estimateGas({ from: this.addr, gasPrice: MAGIC_GAS_PRICE }))
+    const send = async () => {
+      await method.send({ from: this.addr, gas, gasPrice: MAGIC_GAS_PRICE })
+    }
+
+    return { gas, send }
   }
 
   public async computeRmLiquidityAmounts(
@@ -191,7 +214,7 @@ export default class Liquidity {
   }
 
   /**
-   * amount = lpTokenValue * tokenBalanceOfPair / totalSupply
+   * amount = (lpTokenValue * tokenBalanceOfPair / totalSupply) - 1
    */
   private async tokenRmAmount(props: {
     token: Address
@@ -216,6 +239,63 @@ if (import.meta.vitest) {
 
     test('for 7', () => {
       expect(minByDesired(7 as WeiNumStrBn)).toEqual('7')
+    })
+  })
+
+  describe('detecting eth', () => {
+    const tokens = {
+      native: NATIVE_TOKEN,
+      test1: '0xb9920BD871e39C6EF46169c32e7AC4C698688881' as Address,
+      test2: '0x1CDcD477994e86A11E21C27ca907bEA266EA3A0a' as Address,
+    }
+
+    test('no native tokens', () => {
+      expect(
+        detectEth({
+          tokenA: { addr: tokens.test1, desired: asWei('123') },
+          tokenB: { addr: tokens.test2, desired: asWei('321') },
+        }),
+      ).toBe(null)
+    })
+
+    test('native is the first', () => {
+      expect(
+        detectEth({
+          tokenA: { addr: NATIVE_TOKEN, amount: asWei('123') },
+          tokenB: { addr: tokens.test2, amount: asWei('321') },
+        }),
+      ).toMatchInlineSnapshot(`
+        {
+          "eth": {
+            "addr": "0xae3a8a1D877a446b22249D8676AFeB16F056B44e",
+            "amount": "123",
+          },
+          "token": {
+            "addr": "0x1CDcD477994e86A11E21C27ca907bEA266EA3A0a",
+            "amount": "321",
+          },
+        }
+      `)
+    })
+
+    test('native is the second', () => {
+      expect(
+        detectEth({
+          tokenA: { addr: tokens.test2, computedAmount: asWei('123') },
+          tokenB: { addr: NATIVE_TOKEN, amount: asWei('321') },
+        }),
+      ).toMatchInlineSnapshot(`
+        {
+          "eth": {
+            "addr": "0xae3a8a1D877a446b22249D8676AFeB16F056B44e",
+            "amount": "321",
+          },
+          "token": {
+            "addr": "0x1CDcD477994e86A11E21C27ca907bEA266EA3A0a",
+            "computedAmount": "123",
+          },
+        }
+      `)
     })
   })
 }
