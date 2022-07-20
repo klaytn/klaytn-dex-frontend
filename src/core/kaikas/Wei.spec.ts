a2a79fc42bfb9e5b4c74c7001ff705d9bd110c7f
0x009922 <a.marcius26@gmail.com>
2022-07-20 08:35:41 +0600

    [feat]: implement & test `Wei`

---
+++ src/core/kaikas/Wei.spec.ts
@@ -0,0 +1,78 @@
+import BigNumber from 'bignumber.js'
+import BN from 'bn.js'
+import { test, describe, expect } from 'vitest'
+import Wei from './Wei'
+
+describe.each([0n, 42n, 15n * 10n ** 18n, 99n * 10n ** 40n])('Wei creation & cast: %s', (NUMBER) => {
+  test('from string', () => {
+    const str = NUMBER.toString()
+
+    const wei = new Wei(str)
+
+    expect(wei.asBigNum.toFixed()).toEqual(str)
+    expect(wei.asBN.toString()).toEqual(str)
+    expect(wei.asStr).toEqual(str)
+  })
+
+  if (NUMBER <= Number.MAX_SAFE_INTEGER) {
+    test('from number', () => {
+      const num = Number(NUMBER)
+      const numStr = String(num)
+
+      const wei = new Wei(num)
+
+      expect(wei.asStr).toEqual(numStr)
+      expect(wei.asBigNum.toString()).toEqual(new BigNumber(num).toString())
+      expect(wei.asBN.toString()).toEqual(new BN(num).toString())
+    })
+  }
+
+  test('from BigNumber', () => {
+    const num = new BigNumber(NUMBER.toString())
+
+    const wei = new Wei(num)
+
+    expect(wei.asStr).toEqual(NUMBER.toString())
+    expect(wei.asBigNum.toFixed()).toEqual(NUMBER.toString())
+    expect(wei.asBN.toString()).toEqual(NUMBER.toString())
+  })
+
+  test('from BN', () => {
+    const num = new BN(NUMBER.toString())
+
+    const wei = new Wei(num)
+
+    expect(wei.asStr).toEqual(NUMBER.toString())
+    expect(wei.asBigNum.toFixed()).toEqual(NUMBER.toString())
+    expect(wei.asBN.toString()).toEqual(NUMBER.toString())
+  })
+})
+
+describe('Construction from a number', () => {
+  test('ok if number is less than max safe integer', () => {
+    const wei = new Wei(42)
+
+    expect(wei.asStr).toEqual('42')
+  })
+
+  test('throws if number is greater than max safe integer', () => {
+    expect(() => new Wei(Number.MAX_SAFE_INTEGER + 42)).toThrowError(/should be a safe integer/)
+    expect(() => new Wei(Number.MAX_SAFE_INTEGER + 1)).toThrowError(/should be a safe integer/)
+  })
+
+  test('throws if number is not an integer', () => {
+    expect(() => new Wei(0.1)).toThrowError(/should be an integer, got: 0\.1/)
+  })
+})
+
+test('Wei in a reactive Vue tree works', () => {
+  const wei = ref(new Wei(512))
+
+  expect(wei.value.asStr).toEqual('512')
+})
+
+test('Wei in a reactive Vue tree is not reactive itself', () => {
+  const wei = ref(new Wei(0))
+
+  expect(isReactive(wei.value)).toBe(false)
+})
--- src/core/kaikas/Wei.ts
+++ src/core/kaikas/Wei.ts
@@ -1,28 +1,44 @@
 import BigNumber from 'bignumber.js'
 import BN from 'bn.js'
+import invariant from 'tiny-invariant'
 
-type WeiValue = number | string | BigNumber | BN | Wei
+export type WeiInputValue = number | string | BigNumber | BN
 
-type Representation = 'number' | 'string' | 'BigNumber' | 'BN'
+type Representation = 'string' | 'BigNumber' | 'BN'
 
 interface RepresentationMap {
-  number: number
   string: string
   BigNumber: BigNumber
   BN: BN
 }
 
+type ReprValue = RepresentationMap[Representation]
+
 type RepresentationMapNullable = {
   [K in keyof RepresentationMap]: null | RepresentationMap[K]
 }
 
-function emptyReprMap(): RepresentationMapNullable {}
+function emptyReprMap(): RepresentationMapNullable {
+  return { string: null, BigNumber: null, BN: null }
+}
 
-function convert<R extends Representation>(value: number | string | BigNumber | BN, repr: R): RepresentationMap[R] {}
+function convert<R extends Representation>(value: ReprValue, repr: R): RepresentationMap[R] {
+  // naive impl: convert value to bigint, then bigint to repr
+
+  const bi =
+    typeof value === 'string' || typeof value === 'number'
+      ? BigInt(value)
+      : BigNumber.isBigNumber(value)
+      ? BigInt(value.toFixed())
+      : BigInt(value.toString())
+
+  return (
+    repr === 'string' ? bi.toString() : repr === 'BigNumber' ? new BigNumber(bi.toString()) : new BN(bi.toString())
+  ) as RepresentationMap[R]
+}
 
 function convertFromExisting<R extends Representation>(map: RepresentationMapNullable, repr: R): RepresentationMap[R] {
   if (map[repr] !== null) return map[repr]!
-  if (map.number !== null) return convert(map.number, repr)
   if (map.string !== null) return convert(map.string, repr)
   if (map.BigNumber !== null) return convert(map.BigNumber, repr)
   if (map.BN !== null) return convert(map.BN, repr)
@@ -35,43 +51,45 @@ function getAndWriteRepr<R extends Representation>(map: RepresentationMapNullabl
   return value
 }
 
-function setRepr(map: RepresentationMapNullable, value: number | string | BigNumber | BN): void {}
+function setRepr(map: RepresentationMapNullable, value: WeiInputValue): void {
+  if (typeof value === 'number') {
+    invariant(Number.isInteger(value), () => `number should be an integer, got: ${value}`)
+    invariant(Number.isSafeInteger(value), 'number should be a safe integer')
+    map.string = String(value)
+  } else if (typeof value === 'string') {
+    map.string = value
+  } else if (BigNumber.isBigNumber(value)) {
+    map.BigNumber = value
+  } else {
+    map.BN = value
+  }
+}
 
 class EmptyWeiError extends Error {
   public readonly message = 'Wei value should not be constructed without initial value'
 }
 
-declare const WeiTag: unique symbol
-
 export default class Wei {
-  private readonly reprs = markRaw(emptyReprMap())
+  #reprs = emptyReprMap()
 
-  /**
-   * For opaqueness
-   */
-  private readonly [WeiTag]!: 'Wei'
-
-  public constructor(value: WeiValue) {
+  public constructor(value: WeiInputValue | Wei) {
+    markRaw(this)
     if (value instanceof Wei) {
-      this.reprs = value.reprs
+      this.#reprs = value.#reprs
     } else {
-      setRepr(this.reprs, value)
+      setRepr(this.#reprs, value)
     }
   }
 
-  public get str(): string {
-    return getAndWriteRepr(this.reprs, 'string')
+  public get asStr(): string {
+    return getAndWriteRepr(this.#reprs, 'string')
   }
 
-  public get num(): number {
-    return getAndWriteRepr(this.reprs, 'number')
+  public get asBigNum(): BigNumber {
+    return getAndWriteRepr(this.#reprs, 'BigNumber')
   }
 
-  public get bignum(): BigNumber {
-    return getAndWriteRepr(this.reprs, 'BigNumber')
-  }
-
-  public get bn(): BN {
-    return getAndWriteRepr(this.reprs, 'BN')
+  public get asBN(): BN {
+    return getAndWriteRepr(this.#reprs, 'BN')
   }
 }
