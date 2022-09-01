181c35bd3b1f34c408f996df27cd11909fd84bce
0x009922 <a.marcius26@gmail.com>
2022-09-01 06:06:48 +0300

    [fix]: typo

--- src/modules/ModuleLiquidity/Remove/ModeDetailed.vue
+++ src/modules/ModuleLiquidity/Remove/ModeDetailed.vue
@@ -18,7 +18,7 @@ const {
 
 <template>
   <div v-if="tokens && symbols">
-    </InputTokenLp
+    <InputTokenLp
       v-model:liquidity-raw="liquidityRaw"
       :tokens="tokens"
       :balance="pairUserBalance"
