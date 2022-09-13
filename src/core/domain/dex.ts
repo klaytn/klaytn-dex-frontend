c2a76146f3cdee6006283702e41bf1a9c807504f
0x009922 <a.marcius26@gmail.com>
2022-09-13 16:43:59 +0300

    [refactor]: use `MulticallPure` in Earn

--- src/core/domain/dex.ts
+++ src/core/domain/dex.ts
@@ -2,8 +2,9 @@ import { AbiLoader } from '../abi'
 import { Address } from '../types'
 import { Agent, AgentPure, CommonAddrs, AgentProvider } from './agent'
 import CommonContracts from './CommonContracts'
-import { EarnPure, Earn } from './earn'
+import { Earn } from './earn'
 import { Liquidity, LiquidityPure } from './liquidity'
+import MulticallPure from './MulticallPure'
 import { Swap, SwapPure } from './swap'
 import { Tokens, TokensPure } from './tokens'
 
@@ -12,45 +13,44 @@ export class DexPure<
   T extends TokensPure = TokensPure,
   S extends SwapPure = SwapPure,
   L extends LiquidityPure = LiquidityPure,
-  E extends EarnPure = EarnPure,
 > {
   public static initAnonymous(props: { provider: AgentProvider; abi: AbiLoader; addrs: CommonAddrs }): DexPure {
     const agent = new AgentPure(props)
     const contracts = new CommonContracts(agent)
-
     const tokens = new TokensPure({ agent, contracts })
     const swap = new SwapPure({ contracts })
     const liquidity = new LiquidityPure({ agent, tokens })
-    const earn = new EarnPure({ agent })
 
-    return new DexPure({ agent, tokens, swap, liquidity, earn })
+    return new DexPure({ agent, tokens, swap, liquidity })
   }
 
   public readonly agent!: A
   public readonly tokens!: T
   public readonly swap!: S
   public readonly liquidity!: L
-  public readonly earn!: E
 
-  protected constructor(props: Pick<DexPure, 'agent' | 'tokens' | 'swap' | 'liquidity' | 'earn'>) {
+  protected constructor(props: Pick<DexPure, 'agent' | 'tokens' | 'swap' | 'liquidity'>) {
     Object.assign(this, props)
   }
 }
 
-export class Dex extends DexPure<Agent, Tokens, Swap, Liquidity, Earn> {
+export class Dex extends DexPure<Agent, Tokens, Swap, Liquidity> {
   public static init(props: { provider: AgentProvider; abi: AbiLoader; addrs: CommonAddrs }, address: Address): Dex {
     const agent = new Agent({ base: props, address })
     const contracts = new CommonContracts(agent)
-
     const tokens = new Tokens({ agent, contracts })
     const swap = new Swap({ agent, contracts })
     const liquidity = new Liquidity({ agent, tokens, contracts })
-    const earn = new Earn({ agent })
+    const multicall = new MulticallPure({ agent })
+    const earn = new Earn({ agent, multicall })
 
     return new Dex({ agent, tokens, swap, liquidity, earn })
   }
 
+  public readonly earn: Earn
+
   protected constructor(props: Pick<Dex, 'agent' | 'tokens' | 'swap' | 'liquidity' | 'earn'>) {
     super(props)
+    this.earn = props.earn
   }
 }
