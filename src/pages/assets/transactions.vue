57a0b352d9f29426901afb43c9ef847923415688
0x009922 <a.marcius26@gmail.com>
2022-09-19 12:54:20 +0300

    [fix]: remove search text field

--- src/pages/assets/transactions.vue
+++ src/pages/assets/transactions.vue
@@ -11,12 +11,6 @@ store.loadTransactions()
 
 <template>
   <div class="h-full flex flex-col">
-    <div class="px-4 pb-4">
-      <KlayTextField label="Search by... addr? block hash? type? symbol?" />
-    </div>
-
-    <hr class="klay-divider my-0">
-
     <ModuleAssetsTransactionsList class="flex-1" />
   </div>
 
