7a095b5124fa1a6c975e7de024168a8a17696dd8
0x009922 <a.marcius26@gmail.com>
2022-10-05 11:54:16 +0800

    [refactor]: shorten code a bit

--- src/plugins/liquidity-remove-router-guard.ts
+++ src/plugins/liquidity-remove-router-guard.ts
@@ -3,10 +3,7 @@ import { Plugin, RouteName } from '@/types'
 export const install: Plugin = ({ router }) => {
   router.beforeEach((to, from, next) => {
     const store = useLiquidityRmSelectionStore()
-    if (to.name === RouteName.LiquidityRemove && !store.isThereSelectionStored) {
-      next({ name: RouteName.Liquidity })
-    } else {
-      next()
-    }
+    if (to.name === RouteName.LiquidityRemove && !store.isThereSelectionStored) next({ name: RouteName.Liquidity })
+    else next()
   })
 }
