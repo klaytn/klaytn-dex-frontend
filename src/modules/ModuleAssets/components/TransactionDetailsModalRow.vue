9e68e31ce966d184996dd5830a86ab73d52f9cc4
0x009922 <a.marcius26@gmail.com>
2022-10-06 10:01:04 +0800

    [style]: fix transaction modal styles

--- src/modules/ModuleAssets/components/TransactionDetailsModalRow.vue
+++ src/modules/ModuleAssets/components/TransactionDetailsModalRow.vue
@@ -1,11 +1,51 @@
+<script setup lang="ts">
+defineProps<{
+  twoLine?: boolean
+}>()
+</script>
+
 <template>
   <div class="flex">
-    <div class="flex-1 font-semibold text-sm">
+    <div class="title flex-1">
       <slot name="title" />
     </div>
 
-    <div class="flex flex-col">
-      <slot name="value" />
-    </div>
+    <template v-if="twoLine">
+      <div class="flex flex-col space-y-2">
+        <div class="value-top">
+          <slot name="value-top" />
+        </div>
+        <div class="value-sub flex justify-end">
+          <slot name="value-sub" />
+        </div>
+      </div>
+    </template>
+
+    <template v-else>
+      <div class="value-top">
+        <slot name="value" />
+      </div>
+    </template>
   </div>
 </template>
+
+<style scoped lang="scss">
+@use '@/styles/vars';
+
+.title {
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 1em;
+}
+
+.value-top {
+  font-weight: 600;
+  font-size: 14px;
+}
+
+.value-sub {
+  font-weight: 500;
+  font-size: 12px;
+  color: vars.$gray2;
+}
+</style>
