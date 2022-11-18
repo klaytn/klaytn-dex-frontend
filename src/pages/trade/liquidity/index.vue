26b22ebfcaf78cf025e97f79398db22b978b07a8
RDMStreet <RDMStreetOne@gmail.com>
2022-11-18 14:54:04 +0300

    [feature]: liquidity quick poll

--- src/pages/trade/liquidity/index.vue
+++ src/pages/trade/liquidity/index.vue
@@ -1,3 +1,11 @@
+<script lang="ts">
+export default {
+  beforeRouteEnter: function (to, from) {
+    to.meta.quickPoll = from.name === RouteName.LiquidityAdd || from.name === RouteName.LiquidityRemove
+  }
+}
+</script>
+
 <script lang="ts" setup>
 import { RouteName } from '@/types'
 </script>
