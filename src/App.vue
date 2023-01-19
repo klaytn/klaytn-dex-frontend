fda47fce6cf9a4bf81fe8b13cf01708014f40c3d
RDMStreet <RDMStreetOne@gmail.com>
2023-01-19 09:37:33 +0700

    [feature][KLD-245]: adding CSP into header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
 import DefaultLayout from './layouts/default.vue'
+import CONFIG from '~config'
 
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
   title: 'Dex',
-  meta: [{ name: 'description', content: 'Decentralized exchange platform' }],
+  meta: [
+    { name: 'description', content: 'Decentralized exchange platform' },
+    {
+      'http-equiv': 'content-security-policy',
+      content: `
+        default-src 'self' ${CONFIG.network.rpcUrl} ${CONFIG.subgraphs.exchange} ${CONFIG.subgraphs.farming} ${CONFIG.subgraphs.staking} ${CONFIG.subgraphs.snapshot};
+        script-src 'self';
+        style-src 'self' 'unsafe-inline' https://fonts.googleapis.com/;
+        font-src 'self' https://fonts.gstatic.com/;
+        img-src 'self';
+        frame-src 'self';
+      `,
+    },
+  ],
   link: [
     {
       rel: 'icon',
